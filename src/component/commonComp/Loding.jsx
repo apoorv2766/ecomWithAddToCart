d3b94b02702e48b9886117c49127030051f58505
apoorv2766 <145976905+apoorv2766@users.noreply.github.com>
2023-12-18 15:06:24 +0530

    added

--- src/component/commonComp/Loding.jsx
+++ src/component/commonComp/Loding.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { InfinitySpin  } from  'react-loader-spinner'
+import { Vortex  } from  'react-loader-spinner'
 
 function Loding() {
   return (
     <div style={{height:"350px", width:"100%"}} className='d-flex justify-content-center align-items-center'>   
-        <InfinitySpin  width='200' color="#4fa94d"/>
+        <Vortex  width='200' height="200" color="#4fa94d"/>
     </div>
   )
 }
